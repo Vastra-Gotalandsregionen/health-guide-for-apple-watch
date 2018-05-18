350b3bf975b878ffccdc3b1296c1c58750e642b6
mikaelhellqvist <mikaelhellqvist@gmail.com>
2018-05-18 15:54:23 +0200

    updated navigation logic

--- vgr WatchKit Extension/InteractionInterfaceController.swift
+++ vgr WatchKit Extension/InteractionInterfaceController.swift
@@ -4,8 +4,11 @@ import Foundation
 class InteractionInterfaceController: WKInterfaceController {
     var interactionHandler = InteractionHandler()
     var currentInteraction: Interaction?
+    var showingGenericAnswer = false
     
     @IBOutlet var answerLabel: WKInterfaceLabel!
+    @IBOutlet var buttonsGroup: WKInterfaceGroup!
+    
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -22,12 +25,31 @@ class InteractionInterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    @IBAction func yesTapped() {
+        buttonsGroup.setHidden(true)
+        
+        if showingGenericAnswer {
+            startListening()
+        } else {
+            scroll(to: answerLabel, at: .top, animated: false)
+            answerLabel.setText("Ok!\n Baserat på din berättelse drar jag följande slutsatser: \n1. Så länge hostan inte är svår och slemmig i flera dagar behöver du inte oroa dig för KOL-sjukdom.\n2. Om hostan inte ger med sig om en vecka så uppsök jourmottagning.\n3. Boka en allergiutredning hos din vårdcentral")
+        }
+    }
+    
+    @IBAction func noTapped() {
+        if showingGenericAnswer {
+            popToRootController()
+        } else {
+            startListening()
+        }
+    }
+    
     // THIS SHOULD LATER BE SENT FROM PHONE
     func setUpInteractions() {
         let keywords = ["testing", "Hosta", "hostar", "andas", "andning", "andetag", "snuva", "snuvig", "förkylning", "förkyld", "flåsar", "flämta", "pusta", "hals", "halsen", "halsont"]
         
         for keyword in keywords {
-            interactionHandler.newInteraction(interaction: Interaction(keyword: keyword, answer: "Ok! Detta är hur jag skulle sammanfatta din hälsa just nu. Du har problem med: \n - hosta \n - snuva \n - eventuellt allergi \n\n Har jag förstått rätt? \n Ja eller Nej?"))
+            interactionHandler.newInteraction(interaction: Interaction(keyword: keyword, answer: "Ok! Detta är hur jag skulle sammanfatta din hälsa just nu. Du har problem med: \n - hosta \n - snuva \n - eventuellt allergi \n\nHar jag förstått rätt?"))
         }
     }
     
@@ -40,7 +62,8 @@ class InteractionInterfaceController: WKInterfaceController {
             if let result = results?.first as? String {
                 self.updateInteraction(question: result)
                 self.showAnswer()
-
+                
+                self.buttonsGroup.setHidden(false)
                 self.updateQuestions(result)
             } else {
                 self.answerLabel.setText(nil)
@@ -58,11 +81,12 @@ class InteractionInterfaceController: WKInterfaceController {
         guard let foundInteraction = self.interactionHandler.findInteraction(question: question) else {
             return
         }
+        
+        showingGenericAnswer = (foundInteraction.keyword == "generic")
         currentInteraction = foundInteraction
     }
     
     private func showAnswer() {
-        
         if let interaction = currentInteraction {
             answerLabel.setText(interaction.answer)
         }
